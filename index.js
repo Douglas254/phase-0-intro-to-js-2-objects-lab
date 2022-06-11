541b7c458067e61db43a0e58f884f3230403160c
Douglas Obara <douglasobara97@gmail.com>
2022-06-11 22:57:45 +0300

    commit tree 891832fff78a1a14914dec7a7db275090c08c6b3

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+let employee = {
+  name: "Douglas",
+  streetAddress: "Kibera",
+};
+
+/*
+    returns an employee with the original key value pairs and the new key value pair
+    it does not modify the original employee, but rather returns a clone with the new data
+*/
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  let objectClone = { ...employee };
+  objectClone[key] = value;
+  return objectClone;
+};
+
+/*
+    updates `employee` with the given `key` and `value` (it is destructive) and returns the entire update
+*/
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+};
+
+/*
+    deletes `key` from a clone of employee and returns the new employee (it is non-destructive)
+    does not modify the original employee (it is non-destructive)
+*/
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  let objectClone = { ...employee };
+  delete objectClone[key];
+  return objectClone;
+};
+
+/*
+    returns employee without the deleted key/value pair
+    modifies the original employee
+*/
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+};
